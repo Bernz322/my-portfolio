44cbcaa45c400e9525fbecbd779cb4d5da22b4f6
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-06-22 11:46:46 +0800

    additional animations

--- src/config/animations.ts
+++ src/config/animations.ts
@@ -1,3 +1,44 @@
+export const sectionAnimateOnView = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      staggerChildren: 0.3,
+      when: "beforeChildren",
+    },
+  },
+};
+
+export const stackAnimateOnView = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.7,
+      staggerChildren: 0.3,
+      when: "beforeChildren",
+    },
+  },
+};
+
+export const projectsIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      ease: "easeIn",
+    },
+  },
+};
+
 export const fadeUp = {
   hidden: {
     y: 20,
@@ -12,6 +53,22 @@ export const fadeUp = {
     },
   },
 };
+
+export const PopUpFast = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      ease: "easeInOut",
+    },
+  },
+};
+
 export const fadeLeft = {
   hidden: {
     x: -20,
