def9f248f0c208104fc80813451371f43d67f422
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-06-27 09:55:10 +0800

    update and compress image assets to save load time

--- src/assets/projects/index.ts
+++ src/assets/projects/index.ts
@@ -1,9 +1,3 @@
-export { default as vacay } from "./vacay-og.png";
-export { default as jbshort } from "./jbshort-og.png";
-export { default as spotify } from "./spotify-og.png";
-export { default as tsismis } from "./tsismis-og.png";
-export { default as thesis } from "./thesis-og.png";
-
 // JBShort
 export { default as LightOGJB } from "./jbshort/og-light.png";
 export { default as DarkOGJB } from "./jbshort/og-dark.png";
